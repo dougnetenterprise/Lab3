301c5ee202f82966d375472d3421709b66cf7195
dougnetenterprise <98271005+dougnetenterprise@users.noreply.github.com>
2022-02-13 18:02:16 -0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            HttpContext.Session.SetString("bottles", Request.Form["bottles"]);
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab3.Models
+{
+    public class Person
+    {
+        public string firstName
+        {
+            get;
+            set;
+        }
+
+        public string lastName
+        {
+            get;
+            set;
+        }
+        public int age
+        {
+            get;
+            set;
+        }
+        public string emailAddress
+        {
+            get;
+            set;
+        }
+        public string password
+        {
+            get;
+            set;
+        }
+        public string description
+        {
+            get;
+            set;
+        }
+    }
+}
